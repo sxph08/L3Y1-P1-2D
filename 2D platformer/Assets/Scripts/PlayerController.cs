7b83548772836c2b77d6781944652b50a8038b04
sxph08 <EVA24000696@HeartofYorkshire.ac.uk>
2024-11-21 11:59:42 +0000

    Foundations

---
+++ 2D platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("UI")]
+    public TMP_Text timerTxt;
+    public float timer;
+
+    [Header("Main")]
+    public float moveSpeed;
+    public float jumpForce;
+    float inputs;
+    public Rigidbody2D rb;
+    public float groundDistance;
+    public LayerMask layerMask;
+
+    RaycastHit2D hit;
+
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerTxt.text = timer.ToString("F2");
+        
+        Movement();
+    }
+
+    void Movement()
+    {
+        inputs = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new UnityEngine.Vector2(inputs * moveSpeed, rb.velocity.y);
+
+        hit = Physics2D.Raycast(transform.position, -transform.up, groundDistance, layerMask);
+        Debug.DrawRay(transform.position, -transform.up * groundDistance, Color.yellow);
+
+        if (hit.collider)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            transform.position = startPos;
+        }
+        if (other.gameObject.CompareTag("Exit"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
